9175040f2001201cfe608d163f7c91ba0a0a99aa
Daniel P <dp@email>
2025-04-01 10:42:09 +0100

    Update error catching MLPInfluence

--- src/uncertainpy/gradual/semantics/modular/MLPBasedInfluence.py
+++ src/uncertainpy/gradual/semantics/modular/MLPBasedInfluence.py
@@ -18,9 +18,12 @@ class MLPBasedInfluence:
             return 1 / (1 + math.exp(-math.log(weight / (1 - weight)) - aggregate))
         except OverflowError:
             warnings.warn(
-                "Overflow error in compute_strength. Returning 0.",
+                "Overflow error in compute_strength.",
             )
-            return 0
+            if -math.log(weight / (1 - weight)) - aggregate > 0:
+                return 0
+            else:
+                return 1
 
     def __str__(self) -> str:
         return __class__.__name__
